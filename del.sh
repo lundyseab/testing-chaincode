6784be7316319af6f9b7e6948074661fa480ad78
root <root@DESKTOP-MLJH5FE.localdomain>
2023-09-25 12:42:35 +0700

    UpdateChaincode Successfully

--- del.sh
+++ del.sh
@@ -1,9 +1,9 @@
-rm -R ./artifact
-rm -R ./crypto-config
+sudo rm -R ./artifact
+sudo rm -R ./crypto-config
 sudo rm -R ./ledger1
 sudo rm -R ./ledger2
-rm -R ./basic.tar.gz
-rm -R ./log.txt
+sudo rm -R ./basic.tar.gz
+sudo rm -R ./log.txt
 
 docker rm -f $(docker ps -aq)
 docker volume prune
